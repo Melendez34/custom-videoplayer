1a3c5b4fae695c0be990845aad511f868f3b1514
Omar Melendez <omar.melendez@tvazteca.com.mx>
2022-05-20 18:55:17 -0500

    Agregando svg y cambios en js y css

--- js/custom-player.js
+++ js/custom-player.js
@@ -1,1 +1,31 @@
-const media = document.querySelector('media');
+//grab DOM elements
+const video = document.querySelector('.video');
+const playAction = document.querySelector('.play');
+const playButtonIcon = document.querySelector('.iconPlay');
+const pauseButtonIcon = document.querySelector('.iconPause');
+const stopButton = document.querySelector('.stop');
+
+//Listen for events
+video.addEventListener('click', playPauseVideo);
+playAction.addEventListener('click', playPauseVideo);
+stopButton.addEventListener('click', stopVideo);
+
+//Utility functions
+function playPauseVideo() {
+  video.paused ? video.play() : video.pause();
+  playButtonToggleIcon();
+}
+function playButtonToggleIcon() {
+  if (video.paused) {
+    playAction.removeChild(pauseButtonIcon);
+    playAction.appendChild(playButtonIcon);
+  } else {
+    playAction.removeChild(playButtonIcon);
+    playAction.prepend(pauseButtonIcon);
+  }
+}
+function stopVideo() {
+  video.pause();
+  video.currentTime = 0;
+  playButtonToggleIcon();
+}
