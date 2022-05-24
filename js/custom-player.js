1fab8d382682791dcfcf6023314638909437edd3
Omar Melendez <omar.melendez@tvazteca.com.mx>
2022-05-24 15:00:13 -0500

    Ajuste en las funciones de javascript

--- js/custom-player.js
+++ js/custom-player.js
@@ -4,17 +4,21 @@ const playAction = document.querySelector('.play');
 const playButtonIcon = document.querySelector('.iconPlay');
 const pauseButtonIcon = document.querySelector('.iconPause');
 const stopButton = document.querySelector('.stop');
+const progressBar = document.querySelector('.progressBar');
 
 //Listen for events
 video.addEventListener('click', playPauseVideo);
+progressBar.addEventListener('change', setVideoProgress);
 playAction.addEventListener('click', playPauseVideo);
 stopButton.addEventListener('click', stopVideo);
+video.addEventListener('timeupdate', updateBarProgress);
 
 //Utility functions
 function playPauseVideo() {
   video.paused ? video.play() : video.pause();
   playButtonToggleIcon();
 }
+
 function playButtonToggleIcon() {
   if (video.paused) {
     playAction.removeChild(pauseButtonIcon);
@@ -24,8 +28,20 @@ function playButtonToggleIcon() {
     playAction.prepend(pauseButtonIcon);
   }
 }
+
 function stopVideo() {
   video.pause();
   video.currentTime = 0;
+  progressBar.value = 0;
   playButtonToggleIcon();
+}
+
+function setVideoProgress() {
+  video.currentTime = Number((progressBar.value * video.duration) / 100);
+}
+
+function updateBarProgress() {
+  progressBar.value = Number((video.currentTime * 100) / video.duration);
+  let minutes = Math.floor(video.currentTime / 60);
+  console.log(minutes);
 }
